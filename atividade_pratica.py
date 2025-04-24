208b81cdc95cfb6cdfeaefffe4d2d29e26ed4205
JuliaSilva16 <juliasilva23101@gmail.com>
2025-04-24 10:37:05 -0300

    atividade pratica objeto

---
+++ atividade_pratica.py
@@ -0,0 +1,54 @@
+#Etapa 1 - Criar uma lista com os nomes de 5 objetos
+objetos = ['garrafa', 'copo', 'prato', 'talher', 'taça']
+print('lista de objetos criada')
+
+#Etapa 2 - Adicione mais um onjeto ao finla da lista
+objetos.append('colher')
+print('Adicionado')
+
+#Etapa 3 - Acesse o objeto que está na 2° posição
+varificacao = objetos[2]
+print('Acessado com sucesso')
+
+#Etapa 4  - Remova um objeto da lista
+objetos.remove('garrafa')
+print("Objeto removido")
+
+#Etapa 5 - Exiba o tamanho da lista
+len(objetos)
+print(len(objetos))
+
+#Etapa 6 - Mostre todos os itens com um laço for
+for objeto in objetos:
+    print(objeto)
+
+#Etapa 7 - Verifique se 'cadeira' está na lista.Se sim remova-a,se não adicione
+verificar = 'cadeira' in objetos
+if 'cadeira' in objetos:
+    objetos.remove('cadeira')
+    print('Objeto removido,lista atualizda:')
+    print(len(objetos))
+else:
+    objetos.append('cadeira')
+    print("Objeto adicionado,lista atualizada:")
+    print(objetos)
+
+#Etapa 8 - Ordene a lista em ordem alfabética    ]
+objetos.sort()
+print('Lista em ordem alfabética:')
+print(objetos)
+
+#Etapa 9 - Exiba o primeiro e o último objeto
+ordem_um = objetos[1]
+print('primeiro objeto:', ordem_um)
+oredem_ultimo = objetos[len(objetos)-1]
+print('ultimo objeto:', oredem_ultimo)
+
+#Etapa 10 - Limpe toda a lista
+objetos.clear()
+print('lista limpa:')
+print(objetos)
+
+
+
+
